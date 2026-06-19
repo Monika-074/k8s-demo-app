fb72b1b30c05b779ecdcc849406902e231fb7dee
Monika-074 <monikanagaraj.704@gmail.com>
2026-06-19 12:49:02 +0530

    Initial commit: Flask app with Kubernetes manifests

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    hostname = socket.gethostname()
+    return f"Hello from Kubernetes v2! Pod hostname: {hostname}\n"
+
+@app.route("/health")
+def health():
+    return "OK\n"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
